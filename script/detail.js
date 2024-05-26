251b053cb21a6ac61e67206fd06758d040072660
Karen015 <karen.aghajanyan.11@gmail.com>
2024-05-26 11:58:38 +0400

    Revert "addList"
    This reverts commit b51035c65b9a2f7c58bdb774148e88b21db8e0f2.

---
+++ script/detail.js
@@ -0,0 +1,29 @@
+const href = window.location.href;
+const idIndex = href.indexOf("?");
+const id = href.slice(idIndex + 1);
+const detailsSection = document.getElementById("detailsSection");
+fetch(`https://5d76bf96515d1a0014085cf9.mockapi.io/product/${id}`)
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    console.log(data)
+    const div = document.createElement("div");
+    div.classList.add("detailsDiv")
+    div.innerHTML = `
+    <img src="${data.photos[0]}" alt="">
+    <div class="description">
+    <h2>${data.name}</h2>
+    <h4>${data.brand}</h4>
+    <h3>${data.price} AMD</h3>
+    <h3>Description</h3>
+    <p>${data.description}</p>     
+    <h3>Product Preview</h3>
+    <img class="mini" src="${data.photos[0]}" alt="">
+    <img class="mini" src="${data.photos[1]}" alt="">
+    <img class="mini" src="${data.photos[2]}" alt="">
+    </div> 
+    `
+    detailsSection.appendChild(div);
+  });
+
